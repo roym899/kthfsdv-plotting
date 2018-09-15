588788ef17e5c2120e3c945c0b1de11237abe4e1
Leonard Bruns <leonardb@kth.se>
2018-09-15 19:36:43 +0200

    Add functioning PlotWidget

--- plotting/interface.py
+++ plotting/interface.py
@@ -1,5 +1,7 @@
 
 from PyQt4 import QtGui
+from pyqtgraph import PlotWidget
+import numpy as np
 
 class Interface(QtGui.QWidget):
     def __init__(self):
@@ -13,6 +15,13 @@ class Interface(QtGui.QWidget):
         self.reset_button.resize(100, 30)
         self.reset_button.move(10, 10)
         self.reset_button.clicked.connect(self.on_reset_button_clicked)
+
+        self.plotting_widget = PlotWidget(parent=self)
+        self.plotting_widget.resize(510, 460)
+        self.plotting_widget.move(120, 10)
+
+        data = np.array([1,2,3,4])
+        self.plotting_widget.plot(data)
 
         self.show()
 
